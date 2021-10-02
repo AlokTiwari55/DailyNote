13dc142534a9bdeb97b55aa5a62efcb114939ba0
Alok Tiwari <82199167+AlokTiwari55@users.noreply.github.com>
2021-10-02 15:31:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//HINT: You will need to study the classes in the styles.css file to appy styling.
+
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+
+ReactDom.render(<App />, document.getElementById("root"));
